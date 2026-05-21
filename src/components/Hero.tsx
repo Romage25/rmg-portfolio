f83dcaf4cc408d4fab51e66877fecaa150b94638
Rodel Gerodias <rodelgerodias25@gmail.com>
2026-05-21 12:45:36 +0800

    fix: incorrect file path

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,7 @@
 export default function Hero() {
   return (
     <section id="hero" className="hero section dark-background">
-      <img src="assets/img/hero-img.jpg" alt="" data-aos="fade-in" />
+      <img src="/assets/img/hero-img.jpg" alt="" data-aos="fade-in" />
 
       <div
         className="container d-flex flex-column align-items-center justify-content-center text-center"
