55579bb914c8d9cb6fb0737594b38845967294e6
Rodel Gerodias <rodelgerodias25@gmail.com>
2026-05-21 13:31:38 +0800

    chore: remove underline to navigations
    fix: use / instead of index.html

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <header id="header" className="header d-flex align-items-center sticky-top">
       <div className="container-fluid container-xl position-relative d-flex align-items-center justify-content-between">
-        <a href="index.html" className="logo d-flex align-items-center">
+        <a href="/" className="logo d-flex align-items-center">
           {/* Uncomment the line below if you also wish to use an image logo */}
           <img src="assets/img/logo.png" alt="" />
           <h1 className="sitename ">RMG Dev</h1>
